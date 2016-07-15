1a42b3095ea94893986d4d1edbc5d7b99ca8d4e9
Vincent <jingzf0214@gmail.com>
2016-07-15 16:36:39 +0800

    增删改查

--- B0/student.rb
+++ B0/student.rb
@@ -1,18 +1,15 @@
 #!/usr/bin/ruby
 
-def newpass( len )          #产生长度为len的随机字符串（小写）的函数
-chars = ("a".."z").to_a
-newpass = ""
-1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
-return newpass
-end
-
 class Student                                #学生类
    def initialize(id, name, gender, age)
       @stu_id=id
       @stu_name=name
       @stu_gender=gender
       @stu_age=age
+   end
+
+   def getStu_id
+     return @stu_id
    end
 
    def display_details()
@@ -29,16 +26,38 @@ class Student                                #学生类
 
  end
 
+
+ def newpass( len )          #产生长度为len的随机字符串（小写）的函数
+   chars = ("a".."z").to_a
+   newpass = ""
+   1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+   return newpass
+ end
+
+ def add_stu
+   puts "请输入学生信息（格式为 id name gender age 中间以空格隔开）："
+   ar = gets.split
+   st = Student.new(ar[0].to_i, ar[1], ar[2].to_i, ar[3].to_i)
+   return st
+ end
+
+ def store_stu (students)                           #存储学生信息函数
+   aFile = File.new("student.yml", "w+")
+   aFile.syswrite("id   name   gender  age \n")
+   j = 1
+   while j <= students.length
+     aFile.syswrite(students[j-1].info())
+     j += 1
+   end
+   aFile.close
+ end
 i = 1
-checkFile = File.new("student.yml", "r")
-if checkFile
-  checkFile.close
+if File::exists?( "student.yml" )                                          #若student.yml存在，则将按照文件信息来创建学生对象
   arr = IO.readlines("student.yml")
   students = Array.new
-  while i <= 100
+  while i < arr.length
     str = arr[i].split
     students[i-1] = Student.new(str[0].to_i, str[1], str[2].to_i, str[3].to_i)
-    students[i-1].display_details()
     i += 1
   end
 
@@ -50,16 +69,63 @@ else                                        #用循环创建随机学生对象
     s_age = rand(15..20)
     s_gender = 2031 - s_age
     students[i-1] = Student.new(i, s_name, s_gender, s_age)
-    students[i-1].display_details()
     i += 1
   end
+  puts students.length
+  store_stu(students)                                      #存储学生信息
+end
 
-  aFile = File.new("student.yml", "w+")                       #将学生信息写入student.yml 文件中
-  aFile.syswrite("id   name   gender  age \n")
-  j = 1
-  while j < 101
-    aFile.syswrite(students[j].info())
-    j += 1
+puts "                      学生管理"
+puts "1.添加学生  2.删除学生  3.修改学生   4.查询学生  5.学生排序"
+puts "请输入操作前面的编号："
+num = gets
+
+case num.to_i
+when 1
+  students.push( add_stu)
+  store_stu(students)
+when 2
+  i = 0
+  puts "请输入要删除学生的id"
+  n = gets
+  while i < students.length
+    if students[i].getStu_id == n.to_i
+      students.delete_at(i)
+      break
+    end
+    i += 1
   end
-  aFile.close
+  store_stu(students)
+
+when 3
+  i = 0
+  puts "请输入要修改学生的id"
+  n = gets
+  stu = add_stu
+  while i < students.length
+    if students[i].getStu_id == n.to_i
+      students.delete_at(i)
+      students[i] = stu
+      break
+    end
+    i += 1
+  end
+  store_stu(students)
+
+when 4
+  i = 0
+  puts "请输入要查询学生的id"
+  n = gets
+  while i < students.length
+    if students[i].getStu_id == n.to_i
+      puts "id   name   gender  age "
+      puts students[i].info()
+      break
+    end
+    i += 1
+  end
+when 5
+
+else
+  puts "请输入正确的编号"
 end
