3ad0fae96fb24d053b2421f82b2c7551e28e4bd7
VincentJing <jingzf0214@gmail.com>
2016-08-05 11:31:32 +0800

    新建 删除 编辑功能实现

--- B2/myapp.rb
+++ B2/myapp.rb
@@ -5,9 +5,11 @@ ActiveRecord::Base.establish_connection( :adapter => "mysql2", :host => "127.0.0
 :username => "root", :password => "123456", :database => "MESSAGE")    #连接数据库
 
 class User < ActiveRecord::Base
+  has_many :messages
 end
 
 class Message < ActiveRecord::Base
+  belongs_to :users
 end
 
 get '/login' do        #渲染登陆界面
@@ -16,12 +18,13 @@ end
 
 enable :sessions
 
-post '/login' do
+post '/login' do                                       #登陆判断
   username = params[:username]
   password = params[:password]
   a = User.find_by_sql("select * from users where username = '#{username}'")
   if a.length == 1 && a[0].password == password.to_s
     session['id'] = a[0].id
+    session['username'] = a[0].username
     redirect to ('/')
   else
     redirect to ('/login')
@@ -32,14 +35,71 @@ get '/signup' do
   erb :signin
 end
 
-post '/signup' do
-  user = User.new
-  user.username = params[:username]
-  user.password = params[:password]
-  user.save
-  '<center>注册成功！<br>两秒后自动返回登陆界面<meta http-equiv="refresh" content="2;url=/login"></center>'
+post '/signup' do                                       #注册判断
+  a = User.find_by_sql("select * from users where username = '#{params[:username]}'")
+  if a.length == 0
+    user = User.new
+    user.username = params[:username]
+    user.password = params[:password]
+    user.save
+    '<center>注册成功！<br>两秒后自动返回登陆界面<meta http-equiv="refresh" content="2;url=/login"></center>'
+  else
+    '<center>用户名已存在！<br>两秒后自动返回注册界面请重新注册<meta http-equiv="refresh" content="2;url=/signup"></center>'
+  end
+
 end
 
 get '/' do
-  'aaaaaaaaaaaaaaaaaaaaaa'
+  @mess = Message.find_by_sql ("select * from messages")
+  erb :show
+end
+
+get '/add' do                        #新建留言
+  erb :add
+end
+
+post '/add' do                                                   #对新建留言的内容进行判定
+  if params[:message].to_s.length >= 10
+    message = Message.new
+    message.content = params[:message].to_s
+    message.user_id = session['id'].to_i
+    message.created_at = Time.new
+    message.save
+    '<center>添加留言成功！<br>两秒后自动返回<meta http-equiv="refresh" content="2;url=/"></center>'
+  else
+    '<center>添加留言失败，请确认留言字数大于等于10！<br><a href="#" onClick=" javascript :history.back(-1);">重新编辑</a></center>'
+  end
+end
+
+get '/delete/:id' do                          #按照Id删除留言
+  if Message.find(params[:id])
+    Message.delete(params[:id].to_i)
+    '<center>删除成功！<br>两秒后自动返回<meta http-equiv="refresh" content="2;url=/"</center>'
+  else
+    '<center>此id不存在！<br>两秒后自动返回<meta http-equiv="refresh" content="2;url=/"</center>'
+  end
+end
+
+post '/edit/:id' do                    #对主页编辑按钮的响应    对留言内容进行再次编辑
+  @message = Message.find(params[:id])
+  erb :edit
+end
+
+post '/edit' do                               #对再次编辑的内容进行判定
+  if params[:message].to_s.length >= 10
+    a = Message.find(params[:id])
+    a.content = params[:message]
+    a.created_at = Time.new
+    a.save
+    '<center>编辑成功！<br>两秒后自动返回<meta http-equiv="refresh" content="2;url=/">'
+  else
+    '<center>编辑留言失败，请确认留言字数大于等于10！<br><a href="#" onClick=" javascript :history.back(-1);">重新编辑</a></center>'
+  end
+
+end
+
+get '/a/:id' do                               #测试表与表之间的关联
+  user = User.find(params[:id])
+  a = user.messages
+  "#{a.length}"
 end
